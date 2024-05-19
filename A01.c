818b4a86b870280d581398099cb9f3470a707340
Belchior QS <138461976+BelchiorQS@users.noreply.github.com>
2024-05-19 16:35:44 +0100

    Variáveis e tipos de dados
    Atualizando as aulas

--- A01.c
+++ A01.c
@@ -4,6 +4,6 @@ int main(void) {
     int a = 10;
     float b = 20.0;
     char c = 'C';
-    printf("Int: %d, Float: %f, Char: %c\n", a, b, c);
+    printf("Int: %d, Float: %f, Char : %c\n", a, b, c);
     return (0);
 }
